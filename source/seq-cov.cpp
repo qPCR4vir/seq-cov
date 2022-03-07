3449e783e84481656f2aea9f68a47f7d11a54dec
qPCR4vir <arielvina@yahoo.es>
2022-03-07 18:43:05 +0100

    try to change fasta dna5 to dna15

--- source/seq-cov.cpp
+++ source/seq-cov.cpp
@@ -1,19 +1,27 @@
 #include <string>
  
 #include <seqan3/core/debug_stream.hpp>         // for debug_stream
-// #include <seqan3/io/sequence_file/input.hpp>    // for sequence_file_input
-#include <seqan3/io/sequence_file/all.hpp>                // for sequence_file_input and sequence_file_output
+#include <seqan3/io/sequence_file/all.hpp>      // for sequence_file_input and sequence_file_output
+#include <seqan3/alphabet/nucleotide/dna15.hpp> // seqan3::gapped<seqan3::dna15>
+
+struct dna_deg : seqan3::sequence_file_input_default_traits_dna
+{
+    using sequence_alphabet = seqan3::dna15; // instead of dna5
+    template <typename alph>
+    using sequence_container = seqan3::dna15_vector;
+};
  
+
 int main ()
 {
     std::filesystem::path base_dir{"D:/PMS/CoV"}; // get the directory
     seqan3::debug_stream <<1<< base_dir<<"\n";
  
     // Initialise a file input object with a FASTA file.
-    seqan3::sequence_file_input file_in{base_dir/"allnuc0209.fasta"};
-    seqan3::sequence_file_output file_E{base_dir/"E.20220209.fasta"};
-    seqan3::sequence_file_output file_N{base_dir/"N.20220209.fasta"};
-    seqan3::sequence_file_output file_S{base_dir/"S.20220209.fasta"};
+    seqan3::sequence_file_input<dna_deg> file_in{base_dir/"allnuc0209.fasta", seqan3::format_fasta{}};
+    seqan3::sequence_file_output<dna_deg> file_E{base_dir/"E.20220209.fasta"};
+    seqan3::sequence_file_output<dna_deg> file_N{base_dir/"N.20220209.fasta"};
+    seqan3::sequence_file_output<dna_deg> file_S{base_dir/"S.20220209.fasta"};
 
     long e{0L}, n{0L}, s{0L}, t{0L}, m{(1L<<15)-1};
     seqan3::debug_stream << "m= " << m << "\n" ; 
